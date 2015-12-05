7601e12b064e3e17386bc6a53a7e57a052e6dc17
Do Nguyen <doclimbs@gmail.com>
2015-12-05 15:06:36 -0800

    Adding events to the calendar view

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,11 +1,21 @@
 
 $(document).ready(function() {
 
+  $.getJSON('/blocks.json', function(data) {
+    for(obj in data){
+    console.log(data[obj]);
+      $('#calendar').fullCalendar({
+        // defaultView: 'basicWeek',
+        events: [
+          {
+            title: data[obj].category,
+            start: data[obj].beginning,
+            end: data[obj].end
+          }
+        ]
+      })
+    }
+  });
+
     // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-
-    })
-
 });
