f16e32606540d7c868dd49623d9e42d50f4411de
Janik Knittle <janik.knittle@gmail.com>
2015-12-05 15:00:46 -0800

    Create seeds with basic user and blocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ user2 = User.create! :email => 'user2@gmail.com', :password => 'password', :pass
 user3 = User.create! :email => 'user3@gmail.com', :password => 'password', :password_confirmation => 'password'
 user4 = User.create! :email => 'user4@gmail.com', :password => 'password', :password_confirmation => 'password'
 user5 = User.create! :email => 'user5@gmail.com', :password => 'password', :password_confirmation => 'password'
+
+
+users = User.order(:created_at).take(5)
+users.each { |user| user.blocks.create!(beginning: "2015-12-05T20:52:00.000Z", ending: "2015-12-05T22:52:00.000Z", category: "school") }
+users.each { |user| user.blocks.create!(beginning: "2015-12-06T14:52:00.000Z", ending: "2015-12-06T19:52:00.000Z", category: "school") }
+users.each { |user| user.blocks.create!(beginning: "2015-12-07T10:52:00.000Z", ending: "2015-12-07T18:52:00.000Z", category: "school") }
+users.each { |user| user.blocks.create!(beginning: "2015-12-05T11:52:00.000Z", ending: "2015-12-05T13:52:00.000Z", category: "work") }
+users.each { |user| user.blocks.create!(beginning: "2015-12-05T18:52:00.000Z", ending: "2015-12-05T23:52:00.000Z", category: "work") }
